19db64a5904210579b98c8c941691b5ab30de15a
minjeong <kmjeongk@gmail.com>
2023-04-11 10:02:27 +0900

    Rename:recentStudyController -> mainController 이름 변경

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -17,22 +17,22 @@ const getRecentStudies = async (req, res) => {
   }
 };
 
-const getAllUsers = async (req, res) => {
-  try {
-    await client.connect();
-    const userDB = client.db('dev-city').collection('user');
-    const allUsers = await userDB
-      .aggregate([{ $sample: { size: 4 } }])
-      .toArray();
-    if (!allUsers) {
-      res.status(404).send('No users found');
-      return;
-    }
-    res.status(200).json(allUsers);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error retrieving users');
-  }
-};
+// const getAllUsers = async (req, res) => {
+//   try {
+//     await client.connect();
+//     const userDB = client.db('dev-city').collection('user');
+//     const allUsers = await userDB
+//       .aggregate([{ $sample: { size: 4 } }])
+//       .toArray();
+//     if (!allUsers) {
+//       res.status(404).send('No users found');
+//       return;
+//     }
+//     res.status(200).json(allUsers);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send('Error retrieving users');
+//   }
+// };
 
-module.exports = { getRecentStudies, getAllUsers };
+module.exports = getRecentStudies;
