6e24845c1213a270c8ab3089b07484a0a10fc434
JKyEun <j56237@naver.com>
2023-04-16 18:41:24 +0900

    Feat: socket.io 를 통한 웹 소켓 연결 성공

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
 require('dotenv').config();
 
 const server = express();
 const { PORT } = process.env;
+const httpServer = http.createServer(server);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', () => {
+  console.log('소켓 연결 성공');
+});
 
 server.use(cors());
 server.use(express.json({ limit: 5000000 }));
@@ -26,6 +40,6 @@ server.use((err, req, res, next) => {
   res.send(err.message);
 });
 
-server.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`${PORT}번에서 서버가 작동 중입니다!`);
 });
